48021ec9926c51a65b1c6e65c0540a410c1d0993
lefThom <thomas.lefevre.pro@proton.me>
2025-07-25 19:53:01 +0200

    Destructuration des props Gestion du tooltip sur slider (:format probleme)

--- src/components/SingleTuner.vue
+++ src/components/SingleTuner.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref, watch } from 'vue'
+import { ref, watch, toRefs } from 'vue'
 import Slider from '@vueform/slider'
 
+// Props expected from parent component
 const props = defineProps({
   propertyName: String,
   value: Number,
@@ -11,9 +12,14 @@ const props = defineProps({
   units: String
 })
 
-const emit = defineEmits(['update:value'])
-const sliderValue = ref(0)
+// Utilisez toRefs pour déstructurer les props tout en conservant la réactivité
+const { propertyName, value, min, max, step, units } = toRefs(props);
+const sliderValue = ref(props.value)
 
+// Define the emit event for two-way binding
+const emit = defineEmits(['update:value'])
+
+// Keep sliderValue in sync when the parent updates the value prop
 watch(
   () => props.value,
   (newVal) => {
@@ -24,39 +30,50 @@ watch(
   { immediate: true }
 )
 
+// Emit updated value when user interacts with slider/input
 watch(sliderValue, (newVal) => {
   emit('update:value', newVal)
 })
+
+let shouldFormat = false // format is stopping fluid drag
 </script>
 
 <template>
   <div class="single-tuner" style="margin: 1rem 0;">
-    <label> {{ propertyName }} </label>
+    <label :for="`${propertyName}-input`"> {{ propertyName }} </label>
     <Slider class="Slider"
       v-model="sliderValue"
       :min="min"
       :max="max"
       :step="step"
       :lazy="false"
-      :tooltips="false"
-    />
-    <input type="number" v-model="sliderValue" :step=step :min="min" :max="max"/>
+      :format="shouldFormat ? val => `${val} ${units}` : null"
+      show-tooltip="drag"
+    /> <!-- format is stopping fluid drag -->
+    <input
+      :id="`${propertyName}-input`"
+      type="number"
+      v-model="sliderValue"
+      :step=step
+      :min="min"
+      :max="max"/>
     <label class="units">{{ units }}</label>
   </div>
 </template>
 
 <style src="@vueform/slider/themes/default.css"></style>
 <style scoped>
-  .single-tuner label {
-    margin-bottom: 0.25rem;
-    font-weight: 600;
-  }
   .single-tuner {
-    margin: em;
+    margin: 1em;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
+  }
+
+  .single-tuner label {
+    margin-bottom: 0.25rem;
+    font-weight: 600;
   }
 
   .Slider {
